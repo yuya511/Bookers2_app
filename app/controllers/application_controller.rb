ffa32c1e4d1e74e36fda752ef506cc3da9297729
EC2 Default User <ec2-user@ip-172-31-31-35.ec2.internal>
2021-06-18 18:06:00 +0000

    NameLogin Finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #deviseの処理を書く場合、:configure_permitted_parametersを使ってね
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+    end
 end
