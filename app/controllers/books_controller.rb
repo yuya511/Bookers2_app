fe23f86c8e692342e4040b1ea6f0799be5b30038
yuya511 <20015114452y@gmail.com>
2021-06-20 02:55:38 +0000

    book CRUD finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,12 +16,19 @@ class BooksController < ApplicationController
   end
   
   def edit
+    @book = Book.find(params[:id])
   end
   
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
   
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
   
   private
