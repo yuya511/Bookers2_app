99cb05c9db849918f4dcaa3d70c1bc32681c3ea8
yuya511 <20015114452y@gmail.com>
2021-06-20 03:59:20 +0000

    head link finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
   
   resources :books, only: [:create, :show, :index, :edit, :update, :destroy]
